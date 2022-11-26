39aeafdcc09e09e382239e622f02015849448fb0
ABU_MUWAH <abu.muwah@gmail.com>
2022-11-26 16:31:58 +0430

    Updating configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,51 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      borderRadius: {
+        '45px': '45px',
+      },
+      height: {
+        '0.5': '0.5px',
+        '1px': '1px',
+        '2px': '2px',
+        '3px': '3px',
+        '58px': '58px',
+        '158px': '158px',
+        '184px': '184px',
+        '204px': '204px',
+        '208px': '208px',
+        '365px': '365px',
+        '419px': '419px',
+        '500px': '500px',
+        '540px': '540px',
+        '600px': '600px',    
+      },
+      width: {
+        '0.5': '0.5px',
+        '1px': '1px',
+        '2px': '2px',
+        '58px': '58px',
+        '158px': '158px',
+        '184px': '184px',
+        '204px': '204px',
+        '208px': '208px',
+        '265px': '265px',
+        '318px': '318px',
+        '365px': '365px',
+        '374px': '374px',
+        '392px': '392px',
+        '460px': '460px',
+        '483px': '483px',
+        '500px': '500px',
+      },
+      padding: {
+        '13': '13px',
+      },
+      fontFamily: {
+        'poppins': ['poppins']
+      }
+    },
   },
   plugins: [],
 }
